84d6bce4a11ff6b9823d2647d6a6df863beddde6
Hoàng Gia Huy <giahuy2405@gmail.com>
2025-04-30 18:35:03 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def main():
     ql = QuanLySach() # Khởi tạo hệ thống quản lý
     
-    while True: # Menu
+    while True: # Vòng lặp Menu
         print("\nHỆ THỐNG QUẢN LÝ THƯ VIỆN")
         print("1. Thêm tài liệu")
         print("2. Xóa tài liệu")
@@ -15,23 +15,23 @@ def main():
         print("4. Tìm kiếm theo loại")
         print("5. Thoát")
         
-        choice = input("Chọn chức năng: ")
+        choice = input("Chọn chức năng: ") # Người dùng nhập lựa chọn từ 1-5 
         
         if choice == "1":
             them_tai_lieu(ql) # Gọi hàm thêm tài liệu
         elif choice == "2":
-            ma = input("Nhập mã tài liệu cần xóa: ") # Gọi hàm xóa tài liệu
+            ma = input("Nhập mã tài liệu cần xóa: ") # Nhập mã xóa tài liệu
             ql.xoa_tai_lieu(ma)
         elif choice == "3":
             ql.hien_thi_danh_sach() # Hiển thị toàn bộ danh sách
         elif choice == "4":
-            loai = input("Nhập loại cần tìm (Sach/TapChi/Bao): ") # Tìm kiếm theo loại
+            loai = input("Nhập loại cần tìm (Sach/TapChi/Bao): ") # Tìm kiếm theo loại được nhập
             ql.tim_kiem_theo_loai(loai)
         elif choice == "5":
             print("Thao tác hoàn tất.")
-            break
+            break # Kết thúc chương trình
         else:
-            print("Lựa chọn không hợp lệ.")
+            print("Lựa chọn không hợp lệ.") # Lựa chọn nằm ngoài các lựa chọn đã được đề ra
 
 def them_tai_lieu(ql): # Hàm thêm tài liệu
     print("\nChọn loại tài liệu:")
@@ -39,7 +39,7 @@ def them_tai_lieu(ql): # Hàm thêm tài liệu
     print("2. Tạp chí")
     print("3. Báo")
     
-    loai = input("Chọn loại (1-3): ")
+    loai = input("Chọn loại (1-3): ") # Chọn loại tài liệu muốn thêm
     
     # Nhập thông tin chung
     ma = input("Mã tài liệu: ")
@@ -60,12 +60,12 @@ def them_tai_lieu(ql): # Hàm thêm tài liệu
         elif loai == "3":
             # Nhập thông tin riêng của Báo
             ngay_ph = input("Ngày phát hành (dd/mm/yyyy): ")
-            ngay_ph = datetime.strptime(ngay_ph, "%d/%m/%Y")
+            ngay_ph = datetime.strptime(ngay_ph, "%d/%m/%Y") # Chuyển thành đối tượng datetime
             ql.them_tai_lieu(Bao(ma, ten_nxb, so_ban, ngay_ph))
         else:
-            print("Loại không hợp lệ!")
+            print("Loại không hợp lệ.") # Lựa chọn sai
     except ValueError as e:
-        print(f"Lỗi: {e}")
+        print(f"Lỗi: {e}") # Hiển thị người dùng nhập sai
 
 if __name__ == "__main__":
-    main()
+    main() # Chạy file
